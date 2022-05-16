6bec3cdfbf96772caadc5051cedf96d604dcb388
Filipe Mesquita <filipemmesquita95@gmail.com>
2022-05-16 16:17:05 -0300

    implementado parabens e putz Changes to be committed: 	modified:   src/components/footer/Footer.js 	modified:   src/components/styles/footer.css

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,8 +1,42 @@
 import '../styles/footer.css';
+import Party from '../assets/party.png'
+import Sad from '../assets/sad.png'
+
+
 
 export default function Footer(props){
+    function ConclusionCheck(){
+        if(props.results.length===props.questionsAmount){
+            let fail=false;
+            props.results.map(result => {if(result=="wrong"){fail=true;}});
+
+            if (fail==false){
+                return (
+                    <>
+                        
+                        <h2><img src={Party} alt="festa" /><bold>Parabéns!</bold></h2>
+                        <h2>Você não esqueceu de nenhum flashcard!</h2>
+                    </>
+                );
+            }
+            if(fail==true){
+                return (
+                    <>
+                        <h2> <img src={Sad} alt="triste" /><bold>Putz...</bold></h2>
+                        <h2>Ainda faltam alguns... Mas Não desanime!</h2>
+                    </>
+                );
+            }
+
+        }
+        return(
+            <></>  
+          );
+    };
     return(
         <div className="footer">
+
+            <ConclusionCheck />
             <h2>{props.results.length}/{props.questionsAmount} Concluídos</h2>
             <br />
             <div>
