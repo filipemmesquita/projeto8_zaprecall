fb55d8c0d6822aa0e9ab3579c97256cb7a8d6097
Filipe Mesquita <filipemmesquita95@gmail.com>
2022-05-16 17:21:45 -0300

    seleçao de deck implementada  Changes to be committed: 	modified:   src/App.js 	modified:   src/components/landingpage/LandingPage.js

--- src/App.js
+++ src/App.js
@@ -5,25 +5,46 @@ import './components/styles/general.css'
 
 export default function App(){
     const [isStarted, setStarted] = React.useState(false);
-    function start(){
+    const [order, setOrder] = React.useState([]);
+    const [deck, setDeck]=React.useState({});
+
+    let questions=[];
+    let answers=[];
+    const newOrder=[];
+    function start(goal,deckChoice){
+        if(deckChoice=="React"){
+            questions=['O que é JSX?','O React é __ ','Componentes devem iniciar com __ ','Podemos colocar __ dentro do JSX','O ReactDOM nos ajuda __','Usamos o npm para __ ','Usamos props para __','Usamos estado (state) para __'];
+            answers=['Uma extensão de linguagem do JavaScript','uma biblioteca JavaScript para construção de interfaces','letra maiúscula','expressões','interagindo com a DOM para colocar componentes React na mesma','gerenciar os pacotes necessários e suas dependências','passar diferentes informações para componentes','dizer para o React quais informações quando atualizadas devem renderizar a tela novamente'];
+        }
+        if(deckChoice=="Teste"){
+            questions=["p1","p2","p3","p4"]
+            answers=["r1","r2","r3","r4"]
+        }        
+
+        setDeck({questions:questions,answers:answers})
+
+
+        for(let x=0;x<questions.length;x++){
+            newOrder.push(x);
+        }
+        
+        newOrder.sort(() => Math.random() -0.5);
+
+        setOrder(newOrder);
+        
+        console.log(newOrder);
+        console.log(order);
+
         setStarted(true);
     }
-    let questions=['O que é JSX?','O React é __ ','Componentes devem iniciar com __ ','Podemos colocar __ dentro do JSX','O ReactDOM nos ajuda __','Usamos o npm para __ ','Usamos props para __','Usamos estado (state) para __'];
-    let answers=['Uma extensão de linguagem do JavaScript','uma biblioteca JavaScript para construção de interfaces','letra maiúscula','expressões','interagindo com a DOM para colocar componentes React na mesma','gerenciar os pacotes necessários e suas dependências','passar diferentes informações para componentes','dizer para o React quais informações quando atualizadas devem renderizar a tela novamente'];
-    const deck1={questions:questions,answers:answers};
-    const deck2={};
-    const decks=[deck1,deck2];
-    const order=[];
-    for(let x=0;x<questions.length;x++){
-        order.push(x);
-    }
-    console.log(order);
-    order.sort(() => Math.random() -0.5);
-    console.log(order);
+    
+    
+    
+
 
     return(
         <>
-            {(isStarted===false) ? <LandingPage start={start} /> : <Main order={order} deck={deck1} />}
+            {(isStarted===false) ? <LandingPage start={start} /> : <Main order={order} deck={deck} />}
         </>
     );
 }
--- src/components/landingpage/LandingPage.js
+++ src/components/landingpage/LandingPage.js
@@ -1,13 +1,20 @@
+import React from 'react';
 import '../styles/landingpage.css'
 import logo from '../assets/logo.png'
 
 export default function LandingPage(props){
-    
+    const [goal, setGoal] = React.useState("Selecione sua meta de zaps!");
+    const [deckChoice, setDeck] = React.useState("React");
+
     return(
         <div className='content landing'>
             <img src={logo} alt="logo"/>
             <h1>ZapRecall</h1>
-            <button onClick={props.start}>Iniciar Recall!!</button>
+            <select value={deckChoice} onChange={(e) => setDeck(e.target.value)}>
+                <option value="React">React</option>
+                <option value="Teste">Teste</option>
+            </select>
+            <button onClick={()=>props.start(goal,deckChoice)}>Iniciar Recall!!</button>
         </div>
     );
 }
